9b403b99fa79d60a73f72efade47b24ee69a0269
Aapo Kärki <aapokr@gmail.com>
2022-01-22 14:01:52 +0200

    1.14 step 3 final

--- part1/anekdootit/src/App.js
+++ part1/anekdootit/src/App.js
@@ -7,6 +7,9 @@ const Button = ({ handleClick, text }) => (
 )
 
 const App = () => {
+
+  
+
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -16,19 +19,45 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
+
+  // let points = Array(anecdotes.length).fill(0);
+  // let copy = [...points];
+  
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(new Array(anecdotes.length).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
 
   const setRandomAnecdote = () => {
     const randomElement = Math.floor(Math.random() * anecdotes.length);
-    console.log(randomElement);
     setSelected(randomElement);
+    console.log(votes);
+  }
+
+  const addVote = () => {
+    
+    const copy = [...votes]
+    copy[selected] += 1;
+    // points[selected] += 1;
+    setVote(copy);
+    if(copy[selected] > copy[mostVotes]){
+      setMostVotes(selected);
+    }
+    
   }
 
   return (
     <div>
+      <h1>The anecdote with the most votes</h1>
+      <>{anecdotes[mostVotes]}  <b>has {votes[mostVotes]} votes</b></>
+      <br></br>
+
       <Button handleClick={setRandomAnecdote} text = 'next anecdote'/>
-      {anecdotes[selected]}
+      <Button handleClick={addVote} text = 'vote'/>
+      <h1>The anecdote of the day</h1>
+      <>{anecdotes[selected]}  <b>has {votes[selected]} votes</b></>
+      
+
+      
     </div>
   )
 }
