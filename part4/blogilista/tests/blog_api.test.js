9e2b0dee61797e0e3311b5976a6185fecc3a9ee4
Aapo Kärki <aapokr@gmail.com>
2022-05-26 15:32:09 +0300

    4.11

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -53,11 +53,38 @@ test('blog is added to db', async () => {
 
   const blogsAtEnd = await helper.blogsInDb()
   console.log(blogsAtEnd)
-  
+
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
   const titles = blogsAtEnd.map(b => b.title)
 
   expect(titles).toContainEqual('How to add a blog from a test')
+})
+
+test('give undefined likes a value of 0', async () => {
+  const newBlog = {
+    _id: "5a422a851b54a676234d17f7",
+    title: "Blog with likes as undefined",
+    author: "Joojoo Jorma",
+    url: "https://reactpatterns.com/",
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  //console.log(blogsAtEnd)
+  
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  const addedBlogInDb = blogsAtEnd.find(b => b.title === newBlog.title)
+
+  expect(addedBlogInDb.likes).toBeDefined()
+  expect(addedBlogInDb.likes).toEqual(0)
+
+
 })
 
 
