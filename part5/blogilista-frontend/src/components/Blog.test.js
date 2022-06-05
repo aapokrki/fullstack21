3cded6d80a0dee42950010eef9bd5c667c098136
Aapo Kärki <aapokr@gmail.com>
2022-06-05 20:01:26 +0300

    5.14

--- part5/blogilista-frontend/src/components/Blog.test.js
+++ part5/blogilista-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
@@ -21,12 +21,39 @@ describe('<Blog />', () => {
     expect(div).toHaveTextContent(
       'Kiljunvalmistuksen alkeet by Marjatta Marjajuoma'
     )
-    
+
     // Only render title and author by default
     expect(div).not.toHaveTextContent(
       'URL: https://www.marjatanmehu.fi/'
     )
     expect(div).not.toHaveTextContent(
+      'Likes: 1000'
+    )
+  })
+
+  test('renders url and likes when view-button is pressed', async () => {
+    const blog = {
+      title: 'Kiljunvalmistuksen alkeet',
+      author: 'Marjatta Marjajuoma',
+      url: 'https://www.marjatanmehu.fi/',
+      likes: 1000,
+      user: '6295ec065d3f59eb5aec6737'
+
+    }
+    // Render scene
+    const { container } = render(<Blog blog={blog} />)
+    const div = container.querySelector('.blog')
+
+    // Create user and click view button
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    // Expect blog to show more info than by default
+    expect(div).toHaveTextContent(
+      'URL: https://www.marjatanmehu.fi/'
+    )
+    expect(div).toHaveTextContent(
       'Likes: 1000'
     )
   })
